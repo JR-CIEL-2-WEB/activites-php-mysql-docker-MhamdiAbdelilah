492c09b0783a46829910777993e74f3a7e3ce4a7
MhamdiAbdelilah <abdelilah.mhamdi@lycee-jeanrostand.fr>
2024-12-09 18:03:01 +0100

    Exercice 4.3

---
+++ code/exe4.3/main.php
@@ -0,0 +1,16 @@
+<?php
+include 'tri_func.php';
+include 'statistique.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET" && !empty($_GET)) {
+    $list = $_GET['list'];
+    $array = json_decode($list);
+    tri_selection_ref($array);
+    $mid = mediane($array);
+
+    // Output the median as JSON
+    echo json_encode(["median" => $mid ]);
+} else {
+    echo json_encode(["error" => "No data received"]);
+}
+?>
